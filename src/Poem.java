be1de4b5308d73089b33b4fed354ac1138c0895a
Savion Plater <splater@ggc.edu>
2024-04-12 15:29:13 -0400

    :)

--- src/Poem.java
+++ src/Poem.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Poem {
+import java.io.FileWriter;
+import java.io.IOException;
+public class Poem {
+    private String name;
+    private String poet;
+    public Poem() {
+        name = "unknown";
+        poet = "unknown";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPoet() {
+        return poet;
+    }
+
+    public void setPoet(String poet) {
+        this.poet = poet;
+    }
+
+    @Override
+    public String toString() {
+        return "Poem{" +
+                "name='" + name + '\'' +
+                ", poet='" + poet + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Poem poem1 = new Poem();
+        poem1.setName("Eating pancakes");
+        poem1.setPoet("Savion Plater");
+
+        Poem poem2 = new Poem();
+        poem2.setName("Baking pancakes");
+        poem2.setPoet("Savion Plater");
+
+        Poem poem3 = new Poem();
+        poem3.setName("Eat some bacon with some pancakes");
+        poem3.setPoet("Savion Plater");
+
+        Poem[] poems = { poem1, poem2, poem3 };
+
+        try (FileWriter writer = new FileWriter("poems.txt")) {
+            for (Poem poem : poems) {
+                writer.write(poem.getName() + "\n");
+                writer.write(poem.getPoet() + "\n");
+                writer.write("\n");
+            }
+            System.out.println("Poem information has been written to 'poems.txt'.");
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/Poem2.java
+++ src/Poem2.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Poem2 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Poem2 {
+    public static void main(String[] args) {
+        ArrayList<Poem> poems = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("poem2.txt"))) {
+            String line;
+            String name = null;
+            String poet = null;
+
+            while ((line = reader.readLine()) != null) {
+                if (name == null) {
+                    name = line;
+                } else {
+                    poet = line;
+                    Poem poem = new Poem();
+                    poem.setName(name);
+                    poem.setPoet(poet);
+                    poems.add(poem);
+                    name = null;
+                    poet = null;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading from the file.");
+            e.printStackTrace();
+        }
+
+        for (Poem poem : poems) {
+            System.out.println("Poem Name: " + poem.getName());
+            System.out.println("Poet: " + poem.getPoet());
+            System.out.println();
+        }
+    }
 }
